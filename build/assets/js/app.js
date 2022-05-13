63b6007cd2eadf4868e2d41e503961581df9fcbf
Artyom Voitas <artyom.voitas@gmail.com>
2022-05-13 14:59:06 +0300

    commit

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -15,4 +15,26 @@ const swiper = new Swiper('.swiper', {
         slidesPerView: 5,
         },
     },
-});
+})
+
+// console.log(document);
+// var
+// let - хотим менять значения
+// const - не меняем значения
+
+const nav = document.getElementById('nav');
+const navClose = document.getElementById('nav-close');
+const navOpen = document.getElementById('nav-open');
+
+// console.log(nav)
+
+window.ontouchstart = function(event) {
+    if(event.target == navOpen) {
+        nav.classList.add('active');
+    }
+    if(event.target == navClose) {
+        nav.classList.remove('active');
+    }
+}
+
+
